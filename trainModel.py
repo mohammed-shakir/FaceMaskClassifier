fc4c81d40d845417a1f7b7274a8a400dcbfbbb0a
Mohammed Shakir <mohammedshakir010528@gmail.com>
2023-11-11 13:55:17 +0100

    update

--- trainModel.py
+++ trainModel.py
@@ -17,51 +17,51 @@ except Exception as e:
 images_x = images_x/255.0
 
 # Layers
-dense_layers = [1]
-layer_sizes = [128]
-conv_layers = [3]
-epochs = [10, 20, 30, 50, 100]
+dense_layers = [1] # Tested with 0, 1, 2
+layer_sizes = [128] # Tested with 32, 64, 128
+conv_layers = [3] # Tested with 1, 2, 3
+epochs = [10] # Tested with 10, 20, 30, 50, 100
 
 # Create the convolutional neural network
 for dense_layer in dense_layers:
     for layer_size in layer_sizes:
         for conv_layer in conv_layers:
-            # tensorboard --logdir=logs\\
-            name = "{}-conv-{}-nodes-{}-dense-{}".format(conv_layer, layer_size, dense_layer, int(time.time()))
-            print(name)
-            # tensorboard = TensorBoard(log_dir="logs\\{}".format(name))
-            tensorboard = TensorBoard(log_dir=f"logs/{name}")
-            callbacks=[tensorboard]
+            for epoch in epochs:
+                # tensorboard --logdir=logs\\
+                name = "{}-conv-{}-nodes-{}-dense-{}".format(conv_layer, layer_size, dense_layer, int(time.time()))
+                print(name)
+                tensorboard = TensorBoard(log_dir=f"logs/{name}")
+                callbacks=[tensorboard]
 
-            model = Sequential()
+                model = Sequential()
 
-            # Convolutional Layer
-            model.add(Conv2D(layer_size, (3, 3), input_shape=images_x.shape[1:]))
-            model.add(Activation('relu'))
-            model.add(MaxPooling2D(pool_size=(2, 2)))
-
-            # Convolutional Layer
-            for l in range(conv_layer-1):
-                model.add(Conv2D(layer_size, (3, 3)))
+                # Convolutional Layer
+                model.add(Conv2D(layer_size, (3, 3), input_shape=images_x.shape[1:]))
                 model.add(Activation('relu'))
                 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-            # Flatten the data
-            model.add(Flatten())
+                # Convolutional Layer
+                for l in range(conv_layer-1):
+                    model.add(Conv2D(layer_size, (3, 3)))
+                    model.add(Activation('relu'))
+                    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-            # Dense Layer
-            for l in range(dense_layer):
-                model.add(Dense(32))
-                model.add(Activation('relu'))
-                model.add(Dropout(0.2))
+                # Flatten the data
+                model.add(Flatten())
 
-            # Output Layer
-            model.add(Dense(1))
-            model.add(Activation('sigmoid'))
+                # Dense Layer
+                for l in range(dense_layer):
+                    model.add(Dense(32))
+                    model.add(Activation('relu'))
+                    model.add(Dropout(0.2))
 
-            model.compile(loss='binary_crossentropy',
-                          optimizer='adam', metrics=['accuracy'])
+                # Output Layer
+                model.add(Dense(1))
+                model.add(Activation('sigmoid'))
 
-            model.fit(images_x, labels_y, batch_size=32, epochs=10, validation_split=0.1, callbacks=callbacks)
+                model.compile(loss='binary_crossentropy',
+                            optimizer='adam', metrics=['accuracy'])
+
+                model.fit(images_x, labels_y, batch_size=32, epoch=epoch, validation_split=0.1, callbacks=callbacks)
 
 model.save("model.h5")
